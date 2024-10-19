c11a1c6c772a9031ef5e46cce1d81c0069f96fc4
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-10-19 18:49:32 +0500

    first commit

---
+++ src/main/java/com/spring/online_bookstore_management_system/OnlineBookstoreManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spring.online_bookstore_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookstoreManagementSystemApplication.class, args);
+	}
+
+}
